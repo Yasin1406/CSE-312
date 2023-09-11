82f84ecf183cf37530589598e9901e1c83fc757e
Yasin1406 <bsse1406@iit.du.ac.bd>
2023-09-11 14:21:31 +0600

    sep 11

--- lab_06_1406.cpp
+++ lab_06_1406.cpp
@@ -1,8 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int state_num,sym_num;
-void get_regex(vector<vector<string>> pre_regex,vector<vector<string>> &cur_regex,int k);
-void print_regex(vector<vector<string>> regex);
+void get_regex(vector<vector<vector<string>>> &regex,int k);
 int main(){
     cout<<"Enter number of states and symbols: ";
     cin>>state_num>>sym_num;
@@ -44,80 +43,73 @@ int main(){
         }
         cout<<endl;
     }
-    vector<vector<string>> regex;
-    regex.resize(state_num,vector<string>(state_num));
+    vector<vector<vector<string>>> regex;
+    regex.resize(state_num*state_num,vector<vector<string>>(state_num,vector<string>(state_num)));
     for(i=0;i<state_num;i++){
         for(j=0;j<state_num;j++){
-            regex[i][j].push_back('(');
+            regex[0][i][j].push_back('(');
             for(k=0;k<sym_num;k++){
                 if(transition_list[i][k]==states[j]){
-                    regex[i][j].push_back(symbols[k]);
-                    regex[i][j].push_back('+');
+                    regex[0][i][j].push_back(symbols[k]);
+                    regex[0][i][j].push_back('+');
                 }
             }
             if(i==j){
-                regex[i][j]+="E+";
+                regex[0][i][j]+="E+";
             }
-            if(regex[i][j].size()==1){
-                regex[i][j]+="#";
+            if(regex[0][i][j].size()==1){
+                regex[0][i][j]+="#";
             }
-            if(regex[i][j].back()=='+'){
-                regex[i][j].pop_back();
+            if(regex[0][i][j].back()=='+'){
+                regex[0][i][j].pop_back();
             }
-            regex[i][j].push_back(')');
+            regex[0][i][j].push_back(')');
         }
     }
     printf("For k=0\n");
     for(i=0;i<state_num;i++){
         for(j=0;j<state_num;j++){
             printf("R%d%d: ",i+1,j+1);
-            cout<<regex[i][j]<<endl;
+            cout<<regex[0][i][j]<<endl;
         }
     }
     cout<<endl;
-    vector<vector<string>> regex1;
-    get_regex(regex,regex1,1); 
+    for(i=1;i<state_num;i++){
+        get_regex(regex,i);
+    }
     printf("For k=1\n");
     for(i=0;i<state_num;i++){
         for(j=0;j<state_num;j++){
             printf("R%d%d: ",i+1,j+1);
-            cout<<regex1[i][j]<<endl;
+            cout<<regex[1][i][j]<<endl;
         }
     }  
-    vector<vector<string>> regex2;
-    get_regex(regex1,regex2,1); 
     printf("For k=2\n");
     for(i=0;i<state_num;i++){
         for(j=0;j<state_num;j++){
             printf("R%d%d: ",i+1,j+1);
-            cout<<regex2[i][j]<<endl;
+            cout<<regex[2][i][j]<<endl;
         }
     }   
     // print_regex(regex1);
     return 0;
 }
-void get_regex(vector<vector<string>> pre_regex,vector<vector<string>> &cur_regex,int k){
+void get_regex(vector<vector<vector<string>>> &regex,int k){
     int i,j;
-    cur_regex.resize(state_num,vector<string>(state_num));
     for(i=0;i<state_num;i++){
         for(j=0;j<state_num;j++){
             // cur_regex[i].push_back(pre_regex[i][j]);
-            cur_regex[i][j]+=pre_regex[i][j];
-            cur_regex[i][j].push_back('+');
+            regex[k][i][j]+=regex[k-1][i][j];
+            regex[k][i][j].push_back('+');
             // cur_regex[i].push_back(pre_regex[i][k-1]);
-            cur_regex[i][j]+=pre_regex[i][k-1];
-            cur_regex[i][j].push_back('(');
+            regex[k][i][j]+=regex[k-1][i][k-1];
+            regex[k][i][j].push_back('(');
             // cur_regex[i].push_back(pre_regex[k-1][k-1]);
-            cur_regex[i][j]+=pre_regex[k-1][k-1];
-            cur_regex[i][j].push_back(')');
-            cur_regex[i][j].push_back('*');
+            regex[k][i][j]+=regex[k-1][k-1][k-1];
+            regex[k][i][j].push_back(')');
+            regex[k][i][j].push_back('*');
             // cur_regex[i].push_back(pre_regex[k-1][j]);
-            cur_regex[i][j]+=pre_regex[k-1][j];
+            regex[k][i][j]+=regex[k-1][k-1][j];
         }
     }
-}
-void print_regex(vector<vector<string>> regex){
-    int i,j;
-    
-
 }
